d4a082bc7e164c9d45f14204f1463bc534f48d29
jebeen <35005115+jebeen@users.noreply.github.com>
2019-08-08 16:56:43 +0530

    crud with ajax

---
+++ index.php
@@ -0,0 +1,25 @@
+<html>
+<head>
+<link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.4.0/css/bootstrap.min.css">
+<script src="https://ajax.googleapis.com/ajax/libs/jquery/3.4.1/jquery.min.js"></script>
+<script src="https://maxcdn.bootstrapcdn.com/bootstrap/3.4.0/js/bootstrap.min.js"></script>
+<script src="mytest.js"></script>
+<link rel="stylesheet" href="teststyles.css">
+</head>
+<body>
+<div class="crud_ops">
+
+<button class="btn btn-warning" id="create">Create User</button>
+<button class="btn btn-success" id="edit">Edit User</button>
+<button class="btn btn-primary" id="update">Update User</button>
+<button class="btn btn-danger" id="delete">Delete User</button>
+<button class="btn btn-default" id="show">Show Users</button>
+
+
+
+<div id="form-content" style="display:none">
+</div>
+</div>
+</body>
+</html>
+ 
